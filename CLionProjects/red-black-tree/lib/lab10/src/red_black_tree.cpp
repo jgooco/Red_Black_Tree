4d1cc1a9fa690810aa193537f7c7d931c8d5b275
thepi <jovingo1234@gmail.com>
2018-05-01 14:49:46 -0700

    .cpp file now updated from repo

--- CLionProjects/red-black-tree/lib/lab10/src/red_black_tree.cpp
+++ CLionProjects/red-black-tree/lib/lab10/src/red_black_tree.cpp
@@ -1,6 +1,11 @@
 #include "red_black_tree.h"
+#include "iostream"
 
 namespace lab10{
+
+    // AUXILIARY FUNCTION
+    void inOrder_traversal(Node *top);
+
     void rotateleft(Node *&a, Node *&b){
         Node *b_right = b->right;
         b->right = b_right->left;
@@ -16,4 +21,44 @@ namespace lab10{
         b_right->left = b;
         b->parent= b_right;
     }
+
+    void rotateright(Node *&a, Node *&b)
+    {
+
+    }
+
+    void redblacktree::fixviolations(Node *&a, Node *&b)
+    {
+
+    }
+
+    void redblacktree::insert(const int &n)
+    {
+
+    }
+
+    // default constructor
+    redblacktree::redblacktree()
+    {
+        root = nullptr;
+    }
+
+    // prints tree elements by traversing inorder(?)
+    void redblacktree::inorder()
+    {
+        inOrder_traversal(root); // calls aux function
+    }
+
+    // AUXILIARY FUNCTION
+    void inOrder_traversal(Node *top)
+    {
+        if (top == nullptr)
+        {
+            return;
+        }
+        inOrder_traversal(top->left);
+        std::cout << top->data << std::endl;
+        inOrder_traversal(top->right);
+    }
+
 }
