97dd80e776c0492f2e677634d19a4c15d07564f7
karinawong19 <karina.wong@sjsu.edu>
2018-05-01 17:04:27 -0700

    Wrote some lab tests

--- CLionProjects/red-black-tree/test/lab10_tests.cpp
+++ CLionProjects/red-black-tree/test/lab10_tests.cpp
@@ -1,22 +1,22 @@
 #include "gtest/gtest.h"
-#include "../lib/lab08/inc/hash_table.h"
-#include "../lib/lab08/src/sorts.cpp"
+#include "red_black_tree.h"
+#include <iostream>
+//#include "../lib/lab08/inc/hash_table.h"
+//#include "../lib/lab08/src/sorts.cpp"
 //#include "../lib/lab06/inc/doubly_linked_list.h"
 
 class Lab08SortsFixture : public ::testing::Test {
 protected:
     virtual void SetUp() {
-        test_numbers=std::vector<unsigned>({12,8,14,15,18,16,17,19,22,20,4,10,9,13,11,5,7,6,2,1,3});
-
+        //test_numbers=std::vector<unsigned>({12,8,14,15,18,16,17,19,22,20,4,10,9,13,11,5,7,6,2,1,3});
     }
 
 public:
-    lab6::doubly_linked_list *to_sort_short, *to_sort_long;
-    std::vector <unsigned> test_numbers;
-
+    //lab6::doubly_linked_list *to_sort_short, *to_sort_long;
+    //std::vector <unsigned> test_numbers;
 };
 
-
+/*
 class Lab08SortsProfiler : public ::testing::Test {
 protected:
     virtual void SetUp(){
@@ -25,3 +25,23 @@ protected:
 public:
     lab6::doubly_linked_list *list_16, *list_32, *list_64, *list_128, *list_256, *list_512, *list_1024, *list_2048, *list_4096, *list_8192, *list_16384, *list_32768;
 };
+
+ */
+
+TEST(red_black_tree, constructorTest)
+{
+    lab10::redblacktree tree0;
+    EXPECT_EQ(0, tree0.size());
+}
+
+TEST(red_black_tree, insertTest)
+{
+    lab10::redblacktree tree1;
+    tree1.insert(10);
+    tree1.insert(20);
+    tree1.insert(30);
+    tree1.insert(15);
+    tree1.insert(1);
+
+    EXPECT_EQ(5, tree1.size());
+}
